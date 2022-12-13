75bdd0c222cbe2894f2f20ddec423c2dd32fd1ff
Olumide Bambe <loyalsfc@gmail.com>
2022-12-13 02:46:41 +0100

    edit content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,23 +6,9 @@ function About() {
         <article className='max-w-[1040px] mx-auto mb-[100px]' id='about'>
             <h2 data-aos="zoom-out" className='text-[3rem] leading-[120%] mb-4 font-bold text-center'>ABOUT</h2>
             <div className='font-poppin leading-[150%] p-2'>
-                    <p data-aos="fade-in" className='mb-4'>
-                    Shiba rose to become the face of the memecoin world. Proof of Inu is the Shib 2.0 Proof of Inu of meme coins.  Proof of Inu is an ERC20 token built for a meme community of degens.
-                    </p>
-                    <p data-aos="fade-in" data-aos-delay="300" className='mb-4'>
-                    Buying Proof of Inu AKA Shib 2.0 helps investors/holders to have
-                    early access to the ecosystem and future
-                    airdrops.
-                    We are the blockchain of all Inu tokens, the leader of
-                    the degens.
-                    </p>
-                    <p data-aos="fade-in" data-aos-delay="600" className='mb-4'>
-                    We are the blockchain of all Inu tokens. A layer 2
-                    blockchain for all memecoins.
-                    We focus on safety of tokens launched on Proof of
-                    Inu and we make sure tokens on our chain are safe.
-                    </p>
-                    <p data-aos="fade-in" data-aos-delay="900">PROOF OF INU IS THE BLOCKCHAIN BASED ON THE "PROOF OF STAKE", PROOF OF Authority", "PROOF OF History" CONSENSUS MECHANISMS.</p>
+                <p data-aos="fade-in" className='mb-4'>Shiba rose to become the face of the memecoin world. Proof of Inu is the Shib 2.0 Proof of Inu of meme coins. Proof of Inu is an ERC20 token built for a meme community of degens.</p>
+                <p data-aos="fade-in" data-aos-delay="300" className='mb-4'>Buying Proof of Inu AKA Shib 2.0 helps investors to have early access to the ecosystem and future airdrops. We are the blockchain of all Inu tokens, the leader of the degens. Proof of Inu is a layer 2 blockchain for all memecoins.  Proof of Inu's main focus is to ensure tokens on its chain are safe.</p>
+                <p data-aos="fade-in" data-aos-delay="900">Proof of Inu is the blockchain based on the "proof of stake", “proof of authority", "proof of honesty" consensus mechanisms.</p>
             </div>
         </article>
   )
--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -7,7 +7,7 @@ function Tokenomics() {
             <div className="bg-[#FDA005] h-[682px] w-[682px] hidden rounded-full absolute blur-[400px] -right-[341px] -top-[100px]"></div>
             <div className='max-w-[565px] p-2 mb-8 md:mb-0'>
                 <h3 data-aos="slide-down" className='text-[2rem] md:text-[3rem] leading-[120%] font-bold'>Our unique Tokenomics are not to be overlooked</h3>
-                <p data-aos="zoom-in" className='py-4 text-lg leading-[150%] text-[#a2aabb] font-poppin'>$PROOFOFINU is available for trading on Uniswap, one of the largest decentralized exchanges in the world, with more listings to follow in the future as we expand.</p>
+                <p data-aos="zoom-in" className='py-4 text-lg leading-[150%] text-[#a2aabb] font-poppin'>$POI is available for trading on Uniswap, one of the largest decentralized exchanges in the world, with more listings to follow in the future as we expand.</p>
                 <a data-aos="fade-in" target="_blank" href="https://drive.google.com/file/d/1CeWa1pdFfjxGE_JTze0PDCKHTbjxhjuf/view">
                 <button className='py-3 px-12 border-2 border-orange font-semibold text-lg hover:bg-orange hover:text-black rounded-lg'>White Paper <i className="fa-solid fa-arrow-right-long ml-2"></i></button>
                 </a>
